368e9d058698145ac7b7b85e60ccd155b996ff7c
Borhan <brborhan70@gmail.com>
2025-08-30 03:44:41 +0600

    learn variables

---
+++ Variables.py
@@ -0,0 +1,14 @@
+''' What is a Variable?
+# A variable in Python is a named location in memory used to store data.
+Think of it as a container that holds information you can use and modify throughout your program.'''
+
+
+# Variable assignment examples
+name = "Alice"
+age = 25
+height = 5.7
+is_student = True
+
+# Using variables
+print("Name:", name)
+print("Age:", age)
