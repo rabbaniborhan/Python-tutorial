444b757ed99a10b1cae192f9a8418895612a9437
Borhan <brborhan70@gmail.com>
2025-08-30 03:30:17 +0600

    python comment

---
+++ Comment.py
@@ -0,0 +1,11 @@
+# This is a comment. It won't run. it is single line comment
+print("Hello, World!")
+
+
+
+'''
+This is a multi-line comment.
+It can span across several lines.
+Python will ignore this.
+'''
+print("Learning Python is fun!")
