62222d992d3b5d18871c32f938faf2e9664189a3
Borhan <brborhan70@gmail.com>
2025-09-02 23:35:19 +0600

    Operator in python

---
+++ Oparetor.py
@@ -0,0 +1,13 @@
+1##️ Arithmetic Operatorsa,
+a, b = 10, 3
+
+print(a + b)  # 13 (Addition)
+print(a - b)  # 7  (Subtraction)
+print(a * b)  # 30 (Multiplication)
+print(a / b)  # 3.333... (Division - float)
+print(a // b) # 3 (Floor Division)
+print(a % b)  # 1 (Modulus / Remainder)
+print(a ** b) # 1000 (Exponentiation: 10^3)
+
+
+##
