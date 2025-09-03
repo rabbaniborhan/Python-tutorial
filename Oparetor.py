337cdc70eee3a2a95070088333ade8061c1b3353
Borhan <brborhan70@gmail.com>
2025-09-04 05:46:11 +0600

    Operator in python

--- Oparetor.py
+++ Oparetor.py
@@ -1,4 +1,4 @@
-1##️ Arithmetic Operatorsa,
+##️ Arithmetic Operatorsa,
 a, b = 10, 3
 
 print(a + b)  # 13 (Addition)
@@ -10,4 +10,33 @@ print(a % b)  # 1 (Modulus / Remainder)
 print(a ** b) # 1000 (Exponentiation: 10^3)
 
 
-##
+##Comparison (Relational) Operators
+
+x, y = 5, 7
+
+print(x == y)  # False (equal to)
+print(x != y)  # True  (not equal to)
+print(x > y)   # False
+print(x < y)   # True
+print(x >= y)  # False
+print(x <= y)  # True
+
+
+##Logical Operators
+m, n = True, False
+
+print(m and n)  # False
+print(m or n)   # True
+print(not m )    # False
+
+
+
+##Assignment Operators
+
+c = 10
+c += 5   # c = c + 5 → 15
+c -= 3   # 12
+c *= 2   # 24
+c /= 4   # 6.0
+c %= 5   # 1.0
+c **= 3  # 1.0^3 = 1.0
